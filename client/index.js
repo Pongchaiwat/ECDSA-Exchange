e8ecaa9b4b3b2e528f386ff18beb3a4495b7f5ce
Pongchaiwat <gun.pongchaiwat@gmail.com>
2022-01-24 10:55:49 -0500

    firstProject2

--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
 import "./index.scss";
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
 
 const server = "http://localhost:3042";
 
@@ -16,19 +18,45 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 });
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
+
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const senderPrivatekey = document.getElementById("sender-privateKey").value;
+  console.log(sender);
+  console.log(amount);
+  console.log(recipient);
+  console.log(senderPrivatekey);
+  //SIGN
+  const ec = new EC('secp256k1');
+  const key = ec.keyFromPrivate(senderPrivatekey);
+  const message = {
+    amount: amount,
+    recipient:recipient,
+    sender:sender
+  }
+  console.log(message);
+  const msgHash = SHA256(message);
+  console.log(msgHash);
+  console.log(msgHash.words[0]);
+  const signature = key.sign(msgHash.words);
+  console.log(signature);
+  const recid = ec.getKeyRecoveryParam(msgHash.words, signature, key.getPublic());
 
-  const body = JSON.stringify({
-    sender, amount, recipient
+  const body = JSON.stringify({ 
+    sender: sender.trim(), recipient: recipient.trim(), amount, recid, message, signature: {
+      r: signature.r.toString(16),
+      s: signature.s.toString(16)
+    }
   });
+  console.log(body);
 
-  const request = new Request(`${server}/send`, { method: 'POST', body });
+
+  const request = new Request(`${server}/send`, { method: 'POST', body }); //connection with the server and send data
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
     return response.json();
-  }).then(({ balance }) => {
+  }).then(({ balance: balance}) => {
     document.getElementById("balance").innerHTML = balance;
   });
 });
--- server/index.js
+++ server/index.js
@@ -2,17 +2,58 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const SHA256 = require('crypto-js/sha256');
+
+const EC = require('elliptic').ec;
+
+const ec = new EC('secp256k1');
+
+const key = ec.genKeyPair();
+
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+const walletAccount = 4;
+const balances = {};
+
+
+//console.log(publicKey);
+
+for (let i = 1; i < walletAccount; i++) {
+  const key = ec.genKeyPair();
+  const privateKey = key.getPrivate().toString(16);
+  const publicKey = key.getPublic().encode('hex');
+  
+  const address = publicKey.slice(-40);
+  console.log(`(${i})`);
+  console.log(`Address : ${address}`);
+  console.log(`Private Key : ${privateKey}`);
+  console.log(`Public Key : ${publicKey}`);
+
+  
+  const balance = i * 100;
+  
+
+  balances[address] = balance;
+  console.log(`Balance : ${balance}`);
+  
+  
+
 }
+
+console.log('==================');
+console.log('');
+
+// const balances = {
+//   "1": 100,
+//   "2": 50,
+//   "3": 75,
+// }
+
+
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
@@ -20,11 +61,21 @@ app.get('/balance/:address', (req, res) => {
   res.send({ balance });
 });
 
-app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+app.post('/send', (req, res) => { //get data from the client
+  const {sender, signature, recipient, amount, recid} = req.body
+  const message = {amount: amount}
+  const msgHash = SHA256(message).words;
+  const publicKey = ec.recoverPubKey(msgHash, signature, recid);
+  
+  console.log(publicKey);
+  const key1 = ec.keyFromPublic(publicKey);
+  console.log(key1.verify(msgHash, signature));
+
+  if ((publicKey.encode("hex").slice(-40)) === sender) {
+    balances[sender] -= amount;
+    balances[recipient] = (balances[recipient] || 0) + +amount;
+  }
+  res.send({ balance: balances[sender] }); //res: response
 });
 
 app.listen(port, () => {
